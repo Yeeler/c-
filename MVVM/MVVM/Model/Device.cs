cf99f322122e7b569a2d766fdaa24183cafe401b
Leo Zhang <leo.zhang@dti.com.au>
2015-02-24 17:42:34 +0800

    initial check in

---
+++ MVVM/MVVM/Model/Device.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+
+namespace MVVM.Model
+{
+    public class Device
+    {
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public list<IPAddress> IPAddressList { get; set; }
+
+        public Device()
+        {
+            return new Device {Name="Unknow", Type="Unknow", IPAddressList = new IPAddressList()};
+        }
+    }
+}
