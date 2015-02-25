b1ab3d2b1cf55fbdf4bb3e212cff3e712f065200
Leo Zhang <leo.zhang@dti.com.au>
2015-02-25 16:37:09 +0800

    % Added ViewModelBase

---
+++ MVVM/MVVM/ViewModel/ViewModelBase.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ComponentModel;
+
+namespace MVVM.ViewModel
+{
+    public class ViewModelBase : INotifyPropertyChanged, IDisposable
+    {
+        protected ViewModelBase()
+        {
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public virtual void OnPropertyChanged(string propertyName)
+        {
+            var handle = this.PropertyChanged;
+            if(handle != null)
+            {
+                var e = new PropertyChangedEventArgs(propertyName);
+                handle(this, e);
+            }
+        }
+
+        public void Dispose()
+        {
+            this.OnDispose();
+        }
+
+        public virtual void OnDispose()
+        {
+        }
+    }
+}
