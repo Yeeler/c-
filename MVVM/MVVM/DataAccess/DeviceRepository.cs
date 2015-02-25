bf1b7034ccc4885aad8a6290aa4e7c7cffc510b6
Leo Zhang <leo.zhang@dti.com.au>
2015-02-25 11:58:50 +0800

    %on the way middle

---
+++ MVVM/MVVM/DataAccess/DeviceRepository.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using MVVM.Model;
+
+namespace MVVM.DataAccess
+{
+    public class DeviceRepository
+    {
+        readonly List<Device> devices;
+
+        public DeviceRepository()
+        {
+            if (devices == null)
+            {
+                devices = new List<Device>();
+            }
+
+            devices.Add(Device.CreateDevice("Galatica", "Windows", new List<string> { "192.168.1.150", "192.168.1.140" }));
+            devices.Add(Device.CreateDevice("Infinity", "Linux", new List<string> { "192.168.1.110" }));
+            devices.Add(Device.CreateDevice("Prometheus", "MAC", new List<string> { "192.168.1.105" }));
+        }
+
+        public List<Device> GetDevices()
+        {
+            return new List<Device>(devices);
+        }
+    
+    }
+}
--- MVVM/MVVM/Model/Device.cs
+++ MVVM/MVVM/Model/Device.cs
@@ -8,13 +8,17 @@ namespace MVVM.Model
 {
     public class Device
     {
+        public static Device CreateDevice(string name, string type, List<string> ipAddressList)
+        {
+            return new Device { Name = name, Type = type, IPAddressList = ipAddressList };
+        }
+
         public string Name { get; set; }
         public string Type { get; set; }
-        public list<IPAddress> IPAddressList { get; set; }
+        public List<string> IPAddressList { get; set; }
 
-        public Device()
-        {
-            return new Device {Name="Unknow", Type="Unknow", IPAddressList = new IPAddressList()};
-        }
+
+
+
     }
 }
